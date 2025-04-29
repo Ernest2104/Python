dc61ce9e276e438ef36ff8d66d299edcbf4cec8f
Ernesto Villagra <villagra9@gmail.com>
2025-04-28 21:49:29 -0300

    28-4-25/2

---
+++ Dia 3/Practica Listas 1-2-3.py
@@ -0,0 +1,17 @@
+#EJERCICIO 1
+
+mi_lista = [1, 'Ernesto', 'Hola', 5, True]
+print(mi_lista)
+
+#EJERCICIO 2
+
+medios_transporte = ["avión", "auto", "barco", "bicicleta"]
+medios_transporte.append ("motocicleta")
+print (medios_transporte)
+
+#EJERCICIO 3
+
+frutas = ["manzana", "banana", "mango", "cereza", "sandía"]
+eliminado = frutas.pop(2)
+print (eliminado)
+print (frutas)
