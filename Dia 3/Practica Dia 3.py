0c4daf82f30aa83d71d53c326f41a45180e69c54
Ernest21 <villagra9@gmail.com>
2025-04-29 22:02:09 -0300

    29-4-2025/2

--- Dia 3/Practica Dia 3.py
+++ Dia 3/Practica Dia 3.py
@@ -53,3 +53,87 @@ print (lista4)
 
 # DICCIONARIOS
 
+diccionario = {'c1':'valor1', 'c2':'valor2'}
+print (type(diccionario))
+print (diccionario)
+
+resultado = diccionario['c1']
+print(resultado)
+
+cliente = {'nombre':'Juan', 'apellido':'Fuentes', 'peso':80, 'talla': 1.76}
+consulta = cliente['apellido']
+print(consulta)
+
+dic = {'c1': 55, 'c2':[10,20,30], 'c3':{'s1':100, 's2':200}}
+print(dic['c1'])
+print(dic['c2'][0])
+print(dic['c3'])
+print(dic['c3']['s2'])
+
+dic1 = {'c1':['a','b','c'], 'c2':['d','e','f']}
+print(dic1['c2'][1].upper())
+
+dic2 = {1:'a', 2:'b'}
+dic2[3] = 'c'
+print(dic2)
+dic2[2] = 'B'
+print(dic2)
+
+print(dic2.keys())
+print(dic2.values())
+print(dic2.items())
+
+# TUPLAS
+
+mi_tuple = (1,2,3,4)
+mi_tuple2 = (1,2,(10,20),4)
+print(type(mi_tuple))
+print(mi_tuple2[2])
+mi_tuple2 = list(mi_tuple2)
+print(type(mi_tuple2))
+
+t = (1,2,3,1)
+
+w,x,y,z = t
+
+print(w,x,y,z)
+
+# SET
+
+mi_set = set([1,2,3,4,5])
+print(type(mi_set))
+print(mi_set)
+
+otro_set = {1,2,3,4,5}
+print(type(otro_set))
+print(otro_set)
+
+print(len(mi_set))
+print(2 in mi_set)
+
+s1 = set([1,2,3])
+s2 = set([3,4,5])
+s3 = s1.union(s2)
+print(s3)
+
+s1.add(4)
+print(s1)
+s1.remove(1)
+print(s1)
+
+# BOOLEANOS
+
+var1 = True
+var2 = False
+
+print(type(var1))
+print(var1)
+
+numero = 5 > 2 + 3
+print(type(numero))
+print(numero)
+
+lista = [1,2,3,4]
+control = 5 in lista
+print(type(control))
+print(control)
--- Dia 3/Proyecto Dia 3.py
+++ Dia 3/Proyecto Dia 3.py
@@ -1,10 +1,15 @@
 texto = input("Ingrese el texto que quiere analizar: ")
 
-letra1 = input("Ingrese la primera letra: ").lower()
+"""letra1 = input("Ingrese la primera letra: ").lower()
 letra2 = input("Ingrese la segunda letra: ").lower()
-letra3 = input("Ingrese la tercera letra: ").lower()
+letra3 = input("Ingrese la tercera letra: ").lower()"""
 
 # CUANTAS VECES APARECE EN EL TEXTO CADA UNA DE LAS LETRAS
-info1 = list(texto.lower())
-#info_letra_1 = info1
-print (info1)
+"""info1 = list(texto.lower())
+print (f"La cantidad de veces que aparece la letra {letra1} es: {info1.count(letra1)}")
+print (f"La cantidad de veces que aparece la letra {letra2} es: {info1.count(letra2)}")
+print (f"La cantidad de veces que aparece la letra {letra3} es: {info1.count(letra3)}")"""
+
+# CUANTAS PALABRAS HAY EN TOTAL
+palabras = texto.split(" ")
+print(f"La cantidad de palabras del texto son: {len(palabras)} palabras")
