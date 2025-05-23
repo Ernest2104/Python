4e0c791104857b9aee20f95c785fca930c28ff1d
Ernest21 <villagra9@gmail.com>
2025-05-23 13:47:34 -0300

    commit 23/05/2025

--- Dia 6/Proyecto Dia 6.py
+++ Dia 6/Proyecto Dia 6.py
@@ -1,6 +1,6 @@
+import os
 from os import system
 from pathlib import Path
-#import glob
 
 base = Path.home()
 print(base)
@@ -10,29 +10,61 @@ cant_recetas = 0
 for i in Path(ruta).glob("**/*.txt"):
   cant_recetas +=1
 
-system('cls')
+""" system('cls')
 usuario = input('Ingresa tu nombre: ')
 print(f"¡Bienvenido {usuario}!")
 print(f"Las recetas se encuentran en: {ruta}")
-print(f"La cantidad de recetas son: {cant_recetas}\n")
+print(f"La cantidad de recetas son: {cant_recetas}\n") """
 
-print("MENÚ:\n")
-print("[1] - Leer receta")
-print("[2] - Crear receta")
-print("[3] - Leer categoría")
-print("[4] - Leer receta")
-print("[5] - Eliminar categoría")
-print("[6] - Finalizar programa")
+def menu():
+  while True:
+    print("MENÚ:\n")
+    print("[1] - Leer receta")
+    print("[2] - Crear receta")
+    print("[3] - Leer categoría")
+    print("[4] - Leer receta")
+    print("[5] - Eliminar categoría")
+    print("[6] - Finalizar programa")
+    
+    opcion = input("\nSeleccione una opción: ")
+    
+    match opcion:
+      case '1':
+        print("Has seleccionado la Opción 1")
+        break
+      case '2':
+        print("Has seleccionado la Opción 2")
+        break
+      case '3':
+        print("Has seleccionado la Opción 3")
+        break
+      case '4':
+        print("Has seleccionado la Opción 4")
+        break
+      case '5':
+        print("Has seleccionado la Opción 5")
+        break
+      case '6':
+        print("Saliendo de la aplicación")
+        break
+      case _:
+        system('cls')
+        print("Opción inválida. Por favor, ingrese una opción correcta (1, 2, 3, 4, 5, 6).\n")
 
-opcion = input("Ingrese su opción: ")
+def obtener_categorias():
+  categoria = os.listdir(ruta)
+  return categoria
 
-while opcion != 6:
-  match opcion:
-    case 1:
-      print("1")
-    case 2:
-      print("")
+#menu()
 
-    case 3:
+def leer_receta():
+  print(f"Que categoria desea ver:\n1-{obtener_categorias()[0]} 2-{obtener_categorias()[1]} 3-{obtener_categorias()[2]} 4-{obtener_categorias()[3]}\n")
+  cat_ele = input("Ingrese la categoria (1,2,3,4): ")
+  if cat_ele == "1":
+    ruta1 = Path(base, 'Desktop', 'Python', 'Recetas', obtener_categorias()[0])
+    print(ruta1)
 
+leer_receta()
+
+  
 
